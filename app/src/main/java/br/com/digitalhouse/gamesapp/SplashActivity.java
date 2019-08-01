f041213c58a227de072536a3fa26c053d5571acb
Erik Antunes <erik.antunes@live.com>
2019-08-01 14:20:20 -0300

    ajustando splash

---
+++ app/src/main/java/br/com/digitalhouse/gamesapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package br.com.digitalhouse.gamesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        irParaMainActivity();
+    }
+
+    private void irParaMainActivity() {
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
